a176ef40c4629f017c71252c874c996bf1d5cd24
JerrelMitchell <jerrelmitchell@ymail.com>
2018-03-28 09:15:29 -0600

    initial commit. begin tests for patron class.

---
+++ test/patron_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+require 'pry'
+
+class PatronTest < Minitest::Test
+  def test_it_exists
+    patron = Patron.new
+    assert_instance_of Patron, patron
+  end
+
+  def test_it_has_a_name
+    patron = Patron.new("Jerrel")
+    assert_equal "Jerrel", patron.name
+  end
+
+  def test_it_can_have_a_different_name
+    patron = Patron.new("Bob")
+    assert_equal "Bob", patron.name
+  end
+
+  def test_it_can_have_interests
+    patron = Patron.new("Jerrel")
+    assert_equal [], patron.interests
+  end
+
+  def test_it_can_add_an_interest
+    patron = Patron.new("Jerrel")
+    patron.add_interests("Hiking")
+
+    assert_equal ["Hiking"], patron.interests
+  end
